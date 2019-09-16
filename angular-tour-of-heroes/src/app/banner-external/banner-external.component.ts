4ae5ed3f51c7bb1f5dccfa007f3593b3441c33ff
unknown <obieda01@email.franklin.edu>
2019-09-16 03:06:43 -0400

    add banner-initial.component component

--- angular-tour-of-heroes/src/app/banner-external/banner-external.component.ts
+++ angular-tour-of-heroes/src/app/banner-external/banner-external.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-banner-external',
+  selector: 'app-banner',
   templateUrl: './banner-external.component.html',
-  styleUrls: ['./banner-external.component.css']
+  styleUrls:  ['./banner-external.component.css']
 })
-export class BannerExternalComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class BannerComponent {
+  title = 'Test Tour of Heroes';
 }
